b3829015bb4a3027e5d36ba322f4e95d50206bff
MattiasBn <josematiasbelson@gmail.com>
2025-10-18 02:24:01 +0100

    ssl

--- logica/config/database.php
+++ logica/config/database.php
@@ -58,9 +58,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA','/etc/ssl/certs/ca-certificates.crt'),
-                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+             PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
+
         ],
 
         'mariadb' => [
