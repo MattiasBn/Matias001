4c9d034cff246b8b04efa12040cf739a9bd80711
MattiasBn <josematiasbelson@gmail.com>
2025-10-22 23:31:53 +0100

    Seu commit

--- visual/next.config.ts
+++ visual/next.config.ts
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // outras configurações aqui...
+
+   images: {
+    domains: ['www.google.com'], // Adicione outros domínios de imagens externas aqui
+  },
   
   allowedDevOrigins: [
     '192.168.137.1', 
