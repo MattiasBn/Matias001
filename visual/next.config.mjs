3615a16269dc6c0610cfb1134b9d9f647abd0948
MattiasBn <josematiasbelson@gmail.com>
2025-11-27 22:11:30 +0000

    btl

--- visual/next.config.mjs
+++ visual/next.config.mjs
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+
+/**@type {import ('next' ).NextConfig;} */
+
+const  nextConfig = {
   output: "standalone",
+
+  experimental: {
+    optimizePackageImports :[
+
+      '@mui/material','@mui/icons-material'
+    ],
+  },
   reactStrictMode: true,
   images: {
     domains: ["www.google.com"], // podes adicionar outras quando precisar
